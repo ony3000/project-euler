c9e2043e292607034eef33fe8e79cd520676797a
Hyeonjong <nianelo4@gmail.com>
2019-11-13 01:15:54 +0900

    Solve problem 17

---
+++ problems_001to025/problem_017/solution.py
@@ -0,0 +1,50 @@
+if __name__ == '__main__':
+    result = 0
+    word_per_number = {
+        0: '',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+        100: 'hundred',
+    }
+    for num in range(1, 1000):
+        word_count = 0
+        hundreds_place = num // 100
+        tens_place = (num % 100) // 10
+        units_place = num % 10
+        if hundreds_place > 0:
+            word_count += len(word_per_number[hundreds_place] + word_per_number[100])
+        if tens_place > 0 or units_place > 0:
+            if hundreds_place > 0:
+                word_count += len('and')
+            if tens_place < 2:
+                word_count += len(word_per_number[tens_place*10+units_place])
+            else:
+                word_count += len(word_per_number[tens_place*10] + word_per_number[units_place])
+        result += word_count
+    result += len('one' + 'thousand')
+    print(result)
