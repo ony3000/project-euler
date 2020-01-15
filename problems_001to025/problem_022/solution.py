34fc5eb9f5d814f18e2fb35f6e40942e94c19b65
Hyeonjong <nianelo4@gmail.com>
2020-01-16 00:39:06 +0900

    Fix file input path

--- problems_001to025/problem_022/solution.py
+++ problems_001to025/problem_022/solution.py
@@ -1,6 +1,10 @@
+import os
+
+PROBLEM_PATH = os.path.dirname(os.path.abspath(__file__))
+
 if __name__ == '__main__':
     result = 0
-    stream = open('./names.txt', 'r')
+    stream = open(f'{PROBLEM_PATH}/names.txt', 'r')
     names = stream.readline().strip().split(',')
     names.sort()
     for order, name in enumerate(names, start=1):
