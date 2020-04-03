250e6176934de8904ccc36f2d66991bb6e05f603
Hyeonjong <nianelo4@gmail.com>
2020-04-03 21:56:57 +0900

    Create class to measure execution time of solution

---
+++ measurement.py
@@ -0,0 +1,32 @@
+from importlib import import_module
+from time import time
+
+class Stopwatch():
+    def __init__(self):
+        self.is_running = False
+        self.laps = []
+        self.precision = 3
+
+    def start(self):
+        self.is_running = True
+        self.laps.append(time())
+
+    def lap(self):
+        if not self.is_running:
+            raise Exception('타이머가 시작되지 않았습니다.')
+        lap_number = len(self.laps)
+        self.laps.append(time())
+        lap_time = round(self.laps[-1] - self.laps[-2], self.precision)
+        print(f'Lap {lap_number}: {lap_time} sec')
+
+    def stop(self):
+        if not self.is_running:
+            raise Exception('타이머가 시작되지 않았습니다.')
+        lap_number = len(self.laps)
+        self.laps.append(time())
+        lap_time = round(self.laps[-1] - self.laps[-2], self.precision)
+        total_time = round(self.laps[-1] - self.laps[0], self.precision)
+        if lap_number > 1:
+            print(f'Final Lap: {lap_time} sec')
+        print(f'Total execution time: {total_time} sec')
+        self.is_running = False
