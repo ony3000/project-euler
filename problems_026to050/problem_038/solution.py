7745610c4159ff220cf344e2c9568b65b9e0d4a2
Hyeonjong <nianelo4@gmail.com>
2020-04-10 00:14:11 +0900

    Solve problem 38

---
+++ problems_026to050/problem_038/solution.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from collections import Counter
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(PROJECT_PATH)
+
+from measurement import Stopwatch
+
+
+class Solution(Stopwatch):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self):
+        answer = 0
+        pandigital_set = Counter('123456789')
+        multiplicand_candidates = [1, 3, *range(5, 10), *range(25, 34), *range(100, 334), *range(5000, 10000)]
+        for multiplicand in multiplicand_candidates:
+            concatenated_product = ''
+            for n in range(1, 10):
+                product = multiplicand * n
+                concatenated_product += str(product)
+                if len(concatenated_product) == 9:
+                    break
+            product_digit_set = Counter(concatenated_product)
+            if product_digit_set == pandigital_set and answer < int(concatenated_product):
+                answer = int(concatenated_product)
+        return answer
+
+if __name__ == '__main__':
+    soln = Solution()
+    result = soln.execute()
+    print(result)
