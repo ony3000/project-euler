25066c35df3b827e10125c5251b9ce3eda8841cb
Hyeonjong <nianelo4@gmail.com>
2020-04-22 10:16:10 +0900

    Solve problem 40

---
+++ problems_026to050/problem_040/solution.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(PROJECT_PATH)
+
+from measurement import Stopwatch
+
+
+class Solution(Stopwatch):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self):
+        answer = 1
+        num = 1
+        next_n = 1
+        digit_length = 0
+        while digit_length < 1000000:
+            digit_length += len(str(num))
+            if digit_length >= next_n:
+                diff = digit_length - next_n
+                nth_digit = int(str(num)[-(1 + diff)])
+                answer *= nth_digit
+                next_n *= 10
+            num += 1
+        return answer
+
+if __name__ == '__main__':
+    soln = Solution()
+    result = soln.execute()
+    print(result)
