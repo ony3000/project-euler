2371967beb9e7287b7c6deed1da7e3c8c94e6754
Hyeonjong <nianelo4@gmail.com>
2020-05-08 00:13:55 +0900

    Solve problem 42

--- measurement.py
+++ measurement.py
@@ -35,7 +35,7 @@ class Stopwatch():
 if __name__ == '__main__':
     watch = Stopwatch()
     problems_per_group = 25
-    solved_problems = 41
+    solved_problems = 42
 
     for num in range(1, solved_problems+1):
         group_num = (num - 1)//problems_per_group + 1
---
+++ problems_026to050/problem_042/solution.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from math import floor, sqrt
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(PROJECT_PATH)
+
+from measurement import Stopwatch
+
+PROBLEM_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+class Solution(Stopwatch):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self):
+        answer = 0
+        stream = open(f'{PROBLEM_PATH}/words.txt', 'r')
+        names = stream.readline().strip().split(',')
+        for name in names:
+            name = name.strip('"')
+            word_value = sum(ord(letter)-ord('A')+1 for letter in name)
+            num = floor(sqrt(word_value * 2))
+            if num * (num+1) // 2 == word_value:
+                answer += 1
+        return answer
+
+if __name__ == '__main__':
+    soln = Solution()
+    result = soln.execute()
+    print(result)
