d30662b0dd833440839e2d3e0b022e9a9ad899d4
Hyeonjong <nianelo4@gmail.com>
2021-12-04 22:51:14 +0900

    Rewrite solutions from problem 46 to 50 by typescript

---
+++ problems_026to050/problem_047/solution.ts
@@ -0,0 +1,23 @@
+import { SolutionFunction } from '../../lib/types';
+import { primeFactorization } from '../../lib/toolbox';
+
+const solution: SolutionFunction = () => {
+  let answer = null;
+
+  const numberOfPrimeFactors = [0, 0, 0, 0];
+  let num = 2;
+
+  while (answer === null) {
+    numberOfPrimeFactors[num % 4] = new Set(primeFactorization(num)).size;
+
+    if (numberOfPrimeFactors.every((primeFactorCount) => (primeFactorCount === 4))) {
+      answer = num - 3;
+    }
+
+    num += 1;
+  }
+
+  return answer;
+};
+
+export default solution;
---
+++ problems_026to050/problem_049/solution.ts
@@ -0,0 +1,46 @@
+import { isPrime } from 'mathjs';
+
+import { SolutionFunction, Dictionary } from '../../lib/types';
+
+const solution: SolutionFunction = () => {
+  let answer: ReturnType<SolutionFunction> = null;
+
+  const primesPerDigits: Dictionary<number[]> = {};
+
+  for (let num = 3; num < 10000; num += 2) {
+    if (num >= 1000 && isPrime(num)) {
+      const prime = num;
+      const digitSet = String(prime).split('');
+      const combination = digitSet.slice().sort().join('');
+
+      primesPerDigits[combination] = primesPerDigits[combination] || [];
+      primesPerDigits[combination].push(prime);
+    }
+  }
+
+  Object.keys(primesPerDigits).every((combination) => {
+    if (primesPerDigits[combination].length >= 3) {
+      const permutablePrimes = primesPerDigits[combination];
+
+      return permutablePrimes.every((a) => permutablePrimes.every((b) => {
+        if (a < b) {
+          const c = 2 * b - a;
+
+          if (permutablePrimes.includes(c) && b !== 4817 && a !== 1487) {
+            answer = String(a) + String(b) + String(c);
+          }
+
+          return answer === null;
+        }
+
+        return true;
+      }));
+    }
+
+    return true;
+  });
+
+  return answer;
+};
+
+export default solution;
