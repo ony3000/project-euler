973616950ab20e6c0aa92f7e11cb95f67dd1772d
Hyeonjong <nianelo4@gmail.com>
2021-12-14 23:08:56 +0900

    Exclude unsolved problems from testing

--- correctness.test.ts
+++ correctness.test.ts
@@ -7,16 +7,18 @@ const solvedProblemCount = 85;
 
 for (let number = 1; number <= solvedProblemCount; number += 1) {
   const groupNumber = Math.floor((number - 1) / ProblemCountPerGroup) + 1;
-
-  const currentNumber = `00${number}`.slice(-3);
   const firstNumberInGroup = `00${(groupNumber - 1) * ProblemCountPerGroup + 1}`.slice(-3);
   const lastNumberInGroup = `00${groupNumber * ProblemCountPerGroup}`.slice(-3);
 
-  test(`Problem #${number}`, async () => {
-    const modulePath = `./problems_${firstNumberInGroup}to${lastNumberInGroup}/problem_${currentNumber}/solution.ts`;
-    const { default: solutionFunction } = await import(modulePath);
-    const correctAnswer = process.env[`PROBLEM_${currentNumber}_ANSWER`];
+  const currentNumber = `00${number}`.slice(-3);
+  const correctAnswer = process.env[`PROBLEM_${currentNumber}_ANSWER`];
 
-    expect(String(solutionFunction())).toBe(correctAnswer);
-  });
+  if (correctAnswer !== undefined && correctAnswer !== '') {
+    test(`Problem #${number}`, async () => {
+      const modulePath = `./problems_${firstNumberInGroup}to${lastNumberInGroup}/problem_${currentNumber}/solution.ts`;
+      const { default: solutionFunction } = await import(modulePath);
+
+      expect(String(solutionFunction())).toBe(correctAnswer);
+    });
+  }
 }
